61bd82ba8d6057ae7ee98e9120c72ba14c231d4c
Stephen Schrauger <stephen@schrauger.com>
2015-10-07 15:22:13 -0400

    include links file in db.php

--- db.php
+++ db.php
@@ -24,13 +24,6 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-<a href="index.html">Check nginx</a><br />
-<a href="index.php">Check PHP</a><br />
-<a href="phpfpm.php">Check php5-fpm</a><br />
-<a href="hhvm.php">Check hhvm</a><br />
-<a href="db.php">Check mysql</a><br />
-<br />
-<a href="//vps1.med.ucf.edu">Switch to VPS1</a><br />
-<a href="//vps2.med.ucf.edu">Switch to VPS2</a><br />
+<?php include "links.php"; ?>
 </body>
 </html>
