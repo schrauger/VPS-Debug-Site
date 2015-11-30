43d6beaaa908e85d5cb5a8ff9d8ed1ff4cf57447
Stephen Schrauger <stephen@schrauger.com>
2015-11-30 07:48:02 -0500

    updates sql query to check cluster size

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 require_once('config.php'); // database credentials
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     header('HTTP/1.1 500 Internal Server Error');
@@ -11,19 +11,25 @@ if ($conn->connect_error) {
 echo "<html>";
 echo "<body>";
 
-$sql = "SELECT " . $dbcolumn . " FROM " . $dbtable;
-$result = $conn->query($sql);
+//$sql = "SELECT " . $dbcolumn . " as 'message' FROM " . $dbname . "." . $dbtable;
+$sql = "SELECT VARIABLE_VALUE as 'message' FROM information_schema.GLOBAL_STATUS WHERE VARIABLE_NAME = 'wsrep_cluster_size'";
+if (!$result = $conn->query($sql)) {
+//	echo "Error";
+	printf("Errormessage: %s\n", $conn->error());
+}
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "message: " . $row["message"];
+        echo "wsrep_cluster_size: " . $row["message"];
     }
 } else {
     echo "0 results";
 }
+
 $conn->close();
 ?>
 <?php include "links.php"; ?>
 </body>
 </html>
+
