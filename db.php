ec53c934e6b7d81f0cdf151b8b52d66e6518b0d5
Matt Vaccaro <MattVaccaro@COMITs-MacBook-Pro.local>
2017-07-21 10:07:36 -0400

    addl changes

--- db.php
+++ db.php
@@ -14,13 +14,13 @@ include "head.php";
 $sql = "SELECT VARIABLE_VALUE as 'message' FROM information_schema.GLOBAL_STATUS WHERE VARIABLE_NAME = 'wsrep_cluster_size'";
 if (!$result = $conn->query($sql)) {
 //	echo "Error";
-	printf("Errormessage: %s\n", $conn->error());
+	printf("<span class=\"output broken\"><i class=\"material-icons\">warning</i>Error Message: %s\n </span>", $conn->error());
 }
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "wsrep_cluster_size: " . $row["message"];
+        echo "<span class=\"output running\"><i class=\"material-icons\">done</i>wsrep_cluster_size: " . $row["message"] .  "</span>";
     }
 } else {
     echo "0 results";
