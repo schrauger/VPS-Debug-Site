b55123a6004f153d0ed0b9a47bf1dd4c962629b6
Matt Vaccaro <MattVaccaro@COMITs-MBP.localdomain>
2017-07-20 22:41:08 -0400

    added index.html changes to links.php file

--- links.php
+++ links.php
@@ -1,22 +1,27 @@
 <?php require_once('config.php'); ?>
-<h1>Is the VPS Broken?</h1>
 
-<span>nginx running</span>
+<h1 id="#top"><span>Grumpy Cat Unvierse.jpg</span>UCF COM VPS Status</h1>
+
+<span class="output running"><i class="material-icons">done</i>nginx is running.</span>
+
+<!-- Uncomment this and/or use style class for errors! -->
+<!-- <span class="output broken"><i class="material-icons">warning</i>nginx warning!</span> -->
 
 <section>
 
-	<section><a href="index.html">Check nginx</a></section>
-	<section><a href="index.php">Check PHP</a></section>
-	<section><a href="phpfpm.php">Check php5-fpm</a></section>
-	<section><a href="hhvm.php">Check hhvm</a></section>
-	<section><a href="db.php">Check mysql</a></section>
+	<section><a href="index.html"><i class="material-icons">dns</i>Check nginx</a></section>
+	<section><a href="index.php"><i class="material-icons">dns</i>Check PHP</a></section>
+	<section><a href="phpfpm.php"><i class="material-icons">dns</i>Check php5-fpm</a></section>
+	<section><a href="hhvm.php"><i class="material-icons">dns</i>Check hhvm</a></section>
+	<section><a href="db.php"><i class="material-icons">dns</i>Check mysql</a></section>
 
 </section>
 
 <nav>
-	<section><a href="//vps1.address.com">Switch to VPS1</a></section>
-	<section><a href="//vps2.address.com">Switch to VPS2</a></section>
+	<section><a href="//vps1.address.com"><i class="material-icons">computer</i>Switch to VPS1</a></section>
+	<section><a href="//vps2.address.com"><i class="material-icons">computer</i>Switch to VPS2</a></section>
 </nav>
+
 <?php
 foreach ($servers as $server) {
   echo "<a href='" . $server['url'] . "'>Switch to " . $server['name'] . "</a><br />";
