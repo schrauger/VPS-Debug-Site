c3616f8b73219572ff67986c7a1537c8c3e654fa
Stephen Schrauger <stephen@schrauger.com>
2021-05-13 15:32:31 -0400

    disk space

---
+++ disk.php
@@ -0,0 +1,16 @@
+<?php include('head.php'); ?>
+
+<span class="output running"><i class="material-icons">done</i><?php
+
+echo "disk: ";
+
+$free = disk_free_space();
+$total = disk_total_space();
+$percent = ($free/$total)*100;
+
+echo $free . " / " . $total . " = " . $percent . "% used";
+?></span>
+
+<?php include('links.php'); ?>
+
+<?php include('footer.php'); ?>
