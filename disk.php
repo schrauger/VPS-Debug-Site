a5244c93b7627cffbc8fa4d47c139f33d6a13f4c
Stephen Schrauger <stephen@schrauger.com>
2021-05-13 15:38:55 -0400

    disk space

--- disk.php
+++ disk.php
@@ -9,7 +9,7 @@ $total = disk_total_space('/');
 $used = $total - $free;
 $percent = ($used/$total)*100;
 
-echo round(($used/(1024*1024)),2) . "MB / " . round(($total/(1024*1024)),2) . "MB = " . round($percent,2) . "% used";
+echo round(($used/(1024*1024*1024)),2) . "GB / " . round(($total/(1024*1024*1024)),2) . "GB = " . round($percent,2) . "% used";
 ?></span>
 
 <?php include('links.php'); ?>
