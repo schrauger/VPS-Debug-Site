8b8db4eed17abe50354c1e8d81937b8e03678d36
Stephen Schrauger <stephen@schrauger.com>
2021-05-13 15:42:40 -0400

    disk used

--- disk.php
+++ disk.php
@@ -5,11 +5,20 @@
 echo "disk: ";
 
 $free = disk_free_space('/');
+$free_string = round($free/(1024*1024*1024),2) . "GB";
 $total = disk_total_space('/');
+$total_string = round($total/(1024*1024*1024),2) . "GB";
 $used = $total - $free;
+$used_string = round($used/(1024*1024*1024),2) . "GB";
 $percent = ($used/$total)*100;
-
-echo round(($used/(1024*1024*1024)),2) . "GB / " . round(($total/(1024*1024*1024)),2) . "GB = " . round($percent,2) . "% used";
+$percent_string = round($percent,2) . "%";
+//echo "<span class='used'>"round(($used/(1024*1024*1024)),2) . "GB / " . round(($total/(1024*1024*1024)),2) . "GB = " . round($percent,2) . "% used";
+echo "
+<span class='used'>{$used_string}</span>
+<span class='divider'>/</span>
+<span class='total'>{$total_string}</span>
+<span class='percentage'>{$percentage_string} used</span>
+";
 ?></span>
 
 <?php include('links.php'); ?>
